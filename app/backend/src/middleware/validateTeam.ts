07e15a85a61745633467f9a2ea586b8ad1cf2139
Flávia <flaviacunha18@hotmail.com>
2022-11-16 02:26:11 -0300

    feat: Realiza req 26

--- app/backend/src/middleware/validateTeam.ts
+++ app/backend/src/middleware/validateTeam.ts
@@ -5,12 +5,16 @@ const teamService = new TeamsService();
 
 const validateTeam = async (req: Request, res: Response, next: NextFunction) => {
   const { homeTeam, awayTeam } = req.body;
-  const team = await teamService.teamsById(homeTeam);
+  const team1 = await teamService.teamsById(homeTeam);
+  const team2 = await teamService.teamsById(awayTeam);
 
-  if (awayTeam === team) {
+  if (awayTeam === team1) {
     return res.status(422).json({
       message: 'It is not possible to create a match with two equal teams',
     });
+  }
+  if (homeTeam !== team1 || awayTeam !== team2) {
+    return res.status(404).json({ message: 'There is no team with such id!' });
   }
   next();
 };
