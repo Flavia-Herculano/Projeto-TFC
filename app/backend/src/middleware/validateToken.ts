aa6635df19cd993a34e8d3afa5b57890a0007370
Flávia <flaviacunha18@hotmail.com>
2022-11-17 17:35:04 -0300

    Realiza a correção para validação do token na rota /matches

--- app/backend/src/middleware/validateToken.ts
+++ app/backend/src/middleware/validateToken.ts
@@ -4,15 +4,12 @@ import Token from '../helpers/token';
 const validateToken = async (req: Request, res: Response, next: NextFunction) => {
   const token = req.header('Authorization') as string;
 
-  if (!token || token === '') {
-    return res.status(400).json({ message: 'token not found' });
-  }
-  const payload = Token.decodedToken(token);
-
-  if (!payload) {
+  try {
+    Token.decodedToken(token);
+    next();
+  } catch (error) {
     return res.status(401).json({ message: 'Token must be a valid token' });
   }
-  next();
 };
 
 export default validateToken;
